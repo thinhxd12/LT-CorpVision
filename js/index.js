6e07bb1ed684303b84e664d0f01a13f6de4baeed
thinhxd12 <thinhxd12@gmail.com>
2021-07-13 22:26:36 +0700

    v1

---
+++ js/index.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var position = $(this).scrollTop();
+        if (position >= 300) {
+            $('.header').addClass('custom-header');
+            $('.header').removeClass('top-header');
+        }
+        else {
+            $('.header').addClass('top-header');
+            $('.header').removeClass('custom-header');
+        }
+    });
+});
+
+$('.slick-carousel').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+  });
